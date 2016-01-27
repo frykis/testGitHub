ca035d43dd3d8f8659fe8a54d429e8e7d4323bfa
Fredrik Fryklund <fryklund.fredrik@gmail.com>
2016-01-27 16:36:12 +0100

    Pruning 3.0

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 def comp_is_int(s):
     return isinstance(s, int)
-    
+
 
 def test_comp_is_int():
     test_number = 2
     assert comp_is_int(test_number) == 1
-#FREDRIK WAS HERE >:3   
+# FREDRIK WAS HERE >:3
